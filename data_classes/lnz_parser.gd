39597617b181825cb2d59480f319962e66d93e7b
Kathleen Morrill-Pirovich <34375512+tabbzi@users.noreply.github.com>
2025-01-26 13:54:25 -0500

    Refactor lnz_parser and generate_balls for overrides

--- data_classes/lnz_parser.gd
+++ data_classes/lnz_parser.gd
@@ -25,6 +25,49 @@ var palette = null
 
 var file_path
 
+func _init(file_path):
+	if file_path == null:
+		return
+	
+	self.file_path = file_path
+	r.compile("[-.\\d]+")
+	str_r.compile("[\\S]+")
+	
+	var file = File.new()
+	if file.file_exists(file_path):
+		file.open(file_path, File.READ)
+	else:
+		print("File not found: " + file_path)
+		return
+	
+	get_texture_list(file)
+	get_palette(file)
+	get_species(file)
+	get_default_scales(file)
+	get_leg_extensions(file)
+	get_body_extension(file)
+	get_face_extension(file)
+	get_ear_extension(file)
+	get_head_enlargement(file)
+	get_feet_enlargement(file)
+	get_omissions(file)
+	get_lines(file)
+	get_polygons(file)
+	get_balls(file)
+
+	get_ball_size_override(file)
+	get_fuzz_override(file)
+	get_add_ball_override(file)
+	get_color_info_override(file)
+	get_outline_color_override(file)
+	
+	get_addballs(file)
+	get_project_balls(file)
+	parse_paintballs(file)
+	parse_moves(file)
+
+	file.close()
+
 func get_next_section(file: File, section_name: String):
 	file.seek(0)
 	var this_line = ""
@@ -71,100 +114,144 @@ func get_parsed_line_strings(file: File, keys: Array):
 		return_array.append(dict)
 	return return_array
 
-func _init(file_path):
-	if (file_path == null):
-		return
-	
-	self.file_path = file_path
-	r.compile("[-.\\d]+")
-	str_r.compile("[\\S]+")
-	var file = File.new()
-	if file.file_exists(file_path):
-		file.open(file_path, File.READ)
+func get_species(file: File):
+	get_next_section(file, "Species")
+	var parsed_lines = get_parsed_lines(file, ["species"])
+	if parsed_lines.size() == 0:
+		species = 2
 	else:
-		print("file " + file_path + " not found")
-		file.close()
-		return
-	
-	var this_line = ""
-	
-	get_texture_list(file)
-	get_palette(file)
-	get_species(file)
-	get_default_scales(file)
-	get_leg_extensions(file)
-	get_body_extension(file)
-	get_face_extension(file)
-	get_ear_extension(file)
-	get_head_enlargement(file)
-	get_feet_enlargement(file)
-	get_omissions(file)
-	get_lines(file)
-	get_polygons(file)
-	get_balls(file)
-	
+		species = parsed_lines[0].species
+	print("Species detected: " + str(species))
+
+func get_texture_list(file: File):
+	get_next_section(file, "Texture List")
+	var parsed_lines = get_parsed_line_strings(file, ["filepath", "transparent_color", "width", "height"])
+	for line in parsed_lines:
+		var filename = line.filepath.get_file()
+		var texture_size = null
+
+		if line.has("width") and line.has("height"):
+			var width = float(line.width) if line.width.is_valid_float() else 256
+			var height = float(line.height) if line.height.is_valid_float() else 256
+			if width != null and height != null:
+				texture_size = Vector2(width, height)
+
+		texture_list.append({filename = filename, transparent_color = line.transparent_color, texture_size = texture_size})
+
+func get_palette(file: File):
+	get_next_section(file, "Palette")
+	var parsed_lines = get_parsed_line_strings(file, ["filepath"])
+	for line in parsed_lines:
+		var filename = line.filepath.get_file()
+		palette = filename + ".png"
+
+func parse_paintballs(file: File):
 	file.seek(0)
-	
-	get_addballs(file)
-	get_project_balls(file)
-		
-	## Get paintballz
-	while this_line != "[Paint Ballz]" and !file.eof_reached():
-		this_line = file.get_line()
-	while(true):
-		this_line = file.get_line()
-		if this_line.empty() or this_line.begins_with("[") or file.eof_reached() or this_line.begins_with("#2"):
+	while file.get_line() != "[Paint Ballz]" and !file.eof_reached():
+		pass
+	while true:
+		var line = file.get_line()
+		if line.empty() or line.begins_with("[") or file.eof_reached():
 			break
-		if this_line.begins_with(";") or this_line.begins_with('#'):
+		if line.begins_with(";") or line.begins_with("#"):
 			continue
-		var split_line = r.search_all(this_line)
+		var split_line = r.search_all(line)
+		if split_line.size() < 11:
+			continue
 		var base = int(split_line[0].get_string())
-		var diameter_percent = int(split_line[1].get_string())
-		var x = float(split_line[2].get_string())
-		var y = float(split_line[3].get_string())
-		var z = float(split_line[4].get_string())
+		var diameter = int(split_line[1].get_string())
+		var position = Vector3(
+			float(split_line[2].get_string()),
+			float(split_line[3].get_string()),
+			float(split_line[4].get_string())
+		)
 		var color = int(split_line[5].get_string())
-		var outline_color = int(split_line[6].get_string())
-		if (outline_color == -1):
-			outline_color = 0
+		var outline_color = int(split_line[6].get_string()) if int(split_line[6].get_string()) != -1 else 0
 		var fuzz = int(split_line[7].get_string())
 		var outline = int(split_line[8].get_string())
 		var texture = int(split_line[10].get_string())
-		var anchored = 0
-		if split_line.size() > 11:
-			anchored = int(split_line[11].get_string())
-		var paintball = PaintBallData.new(base, diameter_percent, Vector3(x,y,z), color, outline_color, outline, fuzz, 0, texture, anchored)
+		var anchored = int(split_line[11].get_string()) if split_line.size() > 11 else 0
+
+		var paintball = PaintBallData.new(base, diameter, position, color, outline_color, outline, fuzz, 0, texture, anchored)
 		var pb_array = self.paintballs.get(base, [])
 		pb_array.append(paintball)
 		self.paintballs[base] = pb_array
-		
-	## Get move data
+
+func parse_moves(file: File):
 	file.seek(0)
-	while !this_line.begins_with("[Move]") and !file.eof_reached():
-		this_line = file.get_line()
-	while(true):
-		this_line = file.get_line()
-		if this_line.empty() or this_line.begins_with("[") or file.eof_reached() or this_line.begins_with("#2"):
+	while file.get_line() != "[Move]" and !file.eof_reached():
+		pass
+	while true:
+		var line = file.get_line()
+		if line.empty() or line.begins_with("[") or file.eof_reached():
 			break
-		if this_line.begins_with(";") or this_line.begins_with('#'):
+		if line.begins_with(";") or line.begins_with("#"):
 			continue
-		var split_line = r.search_all(this_line)
+		var split_line = r.search_all(line)
+		if split_line.size() < 4:
+			continue
 		var base = int(split_line[0].get_string())
-		var x = int(split_line[1].get_string())
-		var y = int(split_line[2].get_string())
-		var z = int(split_line[3].get_string())
-		var relative_ball = base
-		if split_line.size() > 4:
-			relative_ball = int(split_line[4].get_string())
-		var pos = Vector3(x, y, z)
-		var ar = moves.get(base, [])
-		ar.append({position = pos, relative_to = relative_ball})
-		moves[base] = ar
-		var ball = self.balls[base]
-		ball.position = pos
-		
-	file.close()
-	
+		var position = Vector3(
+			int(split_line[1].get_string()),
+			int(split_line[2].get_string()),
+			int(split_line[3].get_string())
+		)
+		var relative_to = int(split_line[4].get_string()) if split_line.size() > 4 else base
+		var move_array = moves.get(base, [])
+		move_array.append({"position": position, "relative_to": relative_to})
+		moves[base] = move_array
+
+func get_project_balls(file: File):
+	get_next_section(file, "Project Ball")
+	var parsed_lines = get_parsed_lines(file, ["base", "projected", "amount"])
+	for line in parsed_lines:
+		project_ball.append({ball = line.projected, base = line.base, amount = line.amount})
+
+func get_balls(file: File):
+	get_next_section(file, "Ballz Info")
+	var parsed_lines = get_parsed_lines(file, ["color", "outline_color", "speckle", "fuzz", "outline", "size", "group", "texture"])
+	if parsed_lines.size() == 0:
+		print("Error: No Ballz Info found.")
+	var i = 0
+	for line in parsed_lines:
+		var bd = BallData.new(
+			line.size, 
+			Vector3.ZERO, 
+			i, 
+			Vector3.ZERO,
+			line.color,
+			line.outline_color, 
+			line.outline, 
+			line.fuzz, 
+			0.0, 
+			line.group, 
+			line.texture)
+		self.balls[i] = bd
+		#print("Added ball " + str(i) + " with size " + str(line.size))
+		i += 1
+
+func get_addballs(file: File):
+	get_next_section(file, "Add Ball")
+	var parsed_lines = get_parsed_lines(file, ["base", "x", "y", "z", "color", "outline_color", "speckle", "fuzz", "group", "outline", "size", "body_area", "add_group", "texture"])
+	var max_ball_num = balls.keys().max() + 1
+	for line in parsed_lines:
+		var pos = Vector3(line.x, line.y, line.z)
+		var ball = AddBallData.new(
+			line.base,
+		 max_ball_num, 
+		line.size, 
+		pos,
+		line.color, 
+		line.outline_color, 
+		line.outline, 
+		line.fuzz,
+		 0, 
+		line.group, 
+		line.body_area, 
+		line.texture)
+		addballs[max_ball_num] = ball
+		max_ball_num += 1
+
 func get_default_scales(file: File):
 	get_next_section(file, "Default Scales")
 	var parsed_lines = get_parsed_lines(file, ["scale"])
@@ -237,86 +324,45 @@ func get_polygons(file: File):
 			line.texture
 		)
 		polygons.append(poly_data)
-		#print(polygons)
 
-func get_balls(file: File):
-	get_next_section(file, "Ballz Info")
-	var parsed_lines = get_parsed_lines(file, ["color", "outline_color", "speckle", "fuzz", "outline", "size", "group", "texture"])
-	if parsed_lines.size() == 0:
-		print("Error: No Ballz Info found.")
-	var i = 0
+func get_ball_size_override(file: File):
+	get_next_section(file, "Ball Size Override")
+	var parsed_lines = get_parsed_lines(file, ["ball", "size"])
 	for line in parsed_lines:
-		var bd = BallData.new(
-			line.size, 
-			Vector3.ZERO, 
-			i, 
-			Vector3.ZERO,
-			line.color,
-			line.outline_color, 
-			line.outline, 
-			line.fuzz, 
-			0.0, 
-			line.group, 
-			line.texture)
-		self.balls[i] = bd
-		#print("Added ball " + str(i) + " with size " + str(line.size))
-		i += 1
+		var ball_data = balls.get(line.ball, null)
+		if ball_data:
+			ball_data.size = line.size
 
-func get_addballs(file: File):
-	get_next_section(file, "Add Ball")
-	var parsed_lines = get_parsed_lines(file, ["base", "x", "y", "z", "color", "outline_color", "speckle", "fuzz", "group", "outline", "size", "body_area", "add_group", "texture"])
-	var max_ball_num = balls.keys().max() + 1
+func get_color_info_override(file: File):
+	get_next_section(file, "Color Info Override")
+	var parsed_lines = get_parsed_lines(file, ["ball", "color", "group", "texture"])
 	for line in parsed_lines:
-		var pos = Vector3(line.x, line.y, line.z)
-		var ball = AddBallData.new(
-			line.base,
-		 max_ball_num, 
-		line.size, 
-		pos,
-		line.color, 
-		line.outline_color, 
-		line.outline, 
-		line.fuzz,
-		 0, 
-		line.group, 
-		line.body_area, 
-		line.texture)
-		addballs[max_ball_num] = ball
-		max_ball_num += 1
-		
-func get_project_balls(file: File):
-	get_next_section(file, "Project Ball")
-	var parsed_lines = get_parsed_lines(file, ["base", "projected", "amount"])
+		var ball_data = balls.get(line.ball, null)
+		if ball_data:
+			ball_data.color = line.color
+			ball_data.group = line.group
+			ball_data.texture = line.texture
+
+func get_outline_color_override(file: File):
+	get_next_section(file, "Outline Color Override")
+	var parsed_lines = get_parsed_lines(file, ["ball", "outline_color"])
 	for line in parsed_lines:
-		project_ball.append({ball = line.projected, base = line.base, amount = line.amount})
+		var ball_data = balls.get(line.ball, null)
+		if ball_data:
+			ball_data.outline_color = line.outline_color
 
-func get_species(file: File):
-	get_next_section(file, "Species")
-	var parsed_lines = get_parsed_lines(file, ["species"])
-	if parsed_lines.size() == 0:
-		species = 2
-	else:
-		species = parsed_lines[0].species
-	print("Species detected: " + str(species))
-
-func get_texture_list(file: File):
-	get_next_section(file, "Texture List")
-	var parsed_lines = get_parsed_line_strings(file, ["filepath", "transparent_color", "width", "height"])
+func get_fuzz_override(file: File):
+	get_next_section(file, "Fuzz Override")
+	var parsed_lines = get_parsed_lines(file, ["ball", "fuzz"])
 	for line in parsed_lines:
-		var filename = line.filepath.get_file()
-		var texture_size = null
+		var ball_data = balls.get(line.ball, null)
+		if ball_data:
+			ball_data.fuzz = line.fuzz
 
-		if line.has("width") and line.has("height"):
-			var width = float(line.width) if line.width.is_valid_float() else 256
-			var height = float(line.height) if line.height.is_valid_float() else 256
-			if width != null and height != null:
-				texture_size = Vector2(width, height)
-
-		texture_list.append({filename = filename, transparent_color = line.transparent_color, texture_size = texture_size})
-
-func get_palette(file: File):
-	get_next_section(file, "Palette")
-	var parsed_lines = get_parsed_line_strings(file, ["filepath"])
+func get_add_ball_override(file: File):
+	get_next_section(file, "Add Ball Override")
+	var parsed_lines = get_parsed_lines(file, ["ball", "base", "x", "y", "z"])
 	for line in parsed_lines:
-		var filename = line.filepath.get_file()
-		palette = filename + ".png"
+		var add_ball_data = addballs.get(line.ball, null)
+		if add_ball_data:
+			add_ball_data.position = Vector3(line.x, line.y, line.z)
